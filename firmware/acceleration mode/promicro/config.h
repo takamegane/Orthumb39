b5f05a801ce943b5f4d0fa02c88f1acf02bf1334
taka <mailmail08260826@gmail.com>
2024-05-27 23:36:27 +0900

    promicro firm update
    promicro用の加速モード追加

---
+++ firmware/acceleration mode/promicro/config.h
@@ -0,0 +1,24 @@
+// Copyright 2024 taka (@taka)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+/* AZ1UBALL settings */
+#define PIMORONI_TRACKBALL_SCALE 1    // default 5
+#define POINTING_DEVICE_ROTATION_90
